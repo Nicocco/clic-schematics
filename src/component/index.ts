32cda5cae542e2e735611dbbca66702a4e152e28
Nicolas Morisseau <nicolas.morisseau53@gmail.com>
2019-10-20 15:36:39 +0200

    [ADD] simple component schematics

---
+++ src/component/index.ts
@@ -0,0 +1,32 @@
+import { Rule, SchematicContext, Tree, apply, url, applyTemplates, move, chain, mergeWith, SchematicsException } from '@angular-devkit/schematics';
+import { strings, normalize } from '@angular-devkit/core';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+export function component(options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+
+    const workspaceConfig = tree.read('/angular.json');
+    if (!workspaceConfig) {
+      throw new SchematicsException('Could not find Angular workspace configuration');
+    }
+
+    // if (options.path === undefined) {
+    //   options.path = `${project.sourceRoot}/${projectType}`;
+    // }
+
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        classify: strings.classify,
+        dasherize: strings.dasherize,
+        name: options.name
+      }),
+      move(normalize(options.path as string))
+    ]);
+
+    return chain([
+      mergeWith(templateSource)
+    ]);
+  };
+}
